5cbbf217284f2fdf58623e6087769e0c9ba338af
Junseok Shin <sjsage522@gmail.com>
2025-04-02 20:25:47 +0900

    Fix package name

--- src/main/java/kr/co/dealmungchi/hotdealbatch/reactive/RedisConfig.java
+++ src/main/java/kr/co/dealmungchi/hotdealbatch/reactive/RedisConfig.java
@@ -1,4 +1,4 @@
-package kr.co.dealmungchi.hotdealbatch.listener;
+package kr.co.dealmungchi.hotdealbatch.reactive;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
